b22e1593fd393b6d1050a9d01951148a9a4cd133
Jiaji_MAC <510046216@qq.com>
2024-03-01 23:03:09 +0800

    rollback

--- site/assets/js/lastupdate.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var lastModified = new Date(document.lastModified);
-    var formattedDate = lastModified.toLocaleDateString("en-SG"); // You can change 'en-US' to your preferred locale
-    document.getElementById('last-modified-date').innerText += formattedDate;
-});
